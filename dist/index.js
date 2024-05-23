0548526e2fc691355f76ff38fa7ada1322f92a5d
A01754412 <88682649+A01754412@users.noreply.github.com>
2024-05-23 17:14:40 -0600

    cambios

--- dist/index.js
+++ dist/index.js
@@ -7,12 +7,12 @@ const Server_1 = __importDefault(require("./provider/Server"));
 const config_1 = require("./config");
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const MacotaController_1 = __importDefault(require("./controllers/MacotaController"));
+const MascotaController_1 = __importDefault(require("./controllers/MascotaController"));
 const ProductoMascotaController_1 = __importDefault(require("./controllers/ProductoMascotaController"));
 const server = new Server_1.default({
     port: config_1.PORT,
     env: config_1.NODE_ENV,
     middlewares: [express_1.default.json(), express_1.default.urlencoded({ extended: true }), (0, cors_1.default)()],
-    controllers: [MacotaController_1.default.instance, ProductoMascotaController_1.default.instance],
+    controllers: [MascotaController_1.default.instance, ProductoMascotaController_1.default.instance],
 });
 server.init();
